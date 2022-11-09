76a304ae3b387470d7630311e2c102381956ae5f
rahulyadav2000 <46553939+rahulyadav2000@users.noreply.github.com>
2022-11-09 19:00:22 +0000

    oxygen level and minor bugs fixed

--- Assets/Rahul/Scripts/PlayerController.cs
+++ Assets/Rahul/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,20 +12,27 @@ public class PlayerController : MonoBehaviour
     public float gravity = -9.81f;
     public float maxOxygen = 100f;
     public float currentOxygen;
-    public float oxygenDepletionRate;
+    public float oxygenDepletionRate = 0.6f;
     public GameManager gmRef;
 
+    public bool isJumping;
+    public bool isMoving;
 
     private Vector3 moveDirection = Vector3.zero;
     private Vector3 velocity;
 
     private PlayerHealth playerHealth;
     public CharacterController characterController;
+
+    public enum States { move, idle }
+    private States states;
+
     // Start is called before the first frame update
     void Start()
     {
         playerHealth = GetComponent<PlayerHealth>();
         currentOxygen = maxOxygen;
+        states = States.idle;
     }
 
     // Update is called once per frame
@@ -37,54 +45,87 @@ public class PlayerController : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.LeftShift))
         {
-            speed += 4f; 
+            speed += 2f;
         }
 
         characterController.Move(moveDirection * speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetAxis("Horizontal") > 0f || Input.GetAxis("Vertical") > 0f || Input.GetAxis("Horizontal") < 0f || Input.GetAxis("Vertical") < 0f)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -gravity);
+            isMoving = true;
+            states = States.move;
+        }
+        else
+        {
+            isMoving = false;
+            states = States.idle;
+            Debug.Log(states);
+        }
+
+        if (transform.localPosition.y <= 2)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -gravity);
+                currentOxygen -= 2;
+                //Debug.Log(currentOxygen);
+            }
         }
         velocity.y += gravity * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime);
 
         Attack();
-        OxygenMeter();
-        
+
+        if (states == States.idle)
+        {
+            //isMoving = false;   
+            currentOxygen -= 0.2f * Time.deltaTime;
+            //Debug.Log("Depleting amount: " + currentOxygen);
+
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (states == States.move)
+        {
+            OxygenMeter();
+        }
+
     }
 
     public void OnTriggerEnter(Collider collision)
-    {
-        if(collision.gameObject.CompareTag("Enemy"))
         {
-            playerHealth.DecreaseHealth(20);
-            Debug.Log(playerHealth.maxHealth);
-        }
+            if(collision.gameObject.CompareTag("Enemy"))
+            {
+                playerHealth.DecreaseHealth(20);
+                Debug.Log(playerHealth.maxHealth);
+            }
 
-        if(collision.gameObject.CompareTag("Health"))
-        {
-            playerHealth.IncreaseHealth(20);
-            Debug.Log(playerHealth.maxHealth);
-        }
+            if(collision.gameObject.CompareTag("Health"))
+            {
+                playerHealth.IncreaseHealth(20);
+                Debug.Log(playerHealth.maxHealth);
+            }
 
-        if (collision.gameObject.CompareTag("Oxygen"))
-        {
-            currentOxygen += 10;
-        }
+            if (collision.gameObject.CompareTag("Oxygen"))
+            {
+                currentOxygen += 10;
+            }
 
-        if (collision.gameObject.CompareTag("Test"))
-        {
-            Debug.Log("Fell into the depths of");
-        }
+            if (collision.gameObject.CompareTag("Test"))
+            {
+                Debug.Log("Fell into the depths of");
+            }
 
-        if (collision.gameObject.CompareTag("Ship"))
-        {
-            Debug.Log("Win");
-            gmRef.win();
-        }
+            if (collision.gameObject.CompareTag("Ship"))
+            {
+                Debug.Log("Win");
+                gmRef.win();
+            }
 
-    }
+        }
 
     public void OnCollisionEnter(Collision collision)
     {
