5a4a9408b835c2b4e061d3a6be69824fd29549cd
sadhaX18 <114281241+sadhaX18@users.noreply.github.com>
2022-11-10 16:16:03 +0000

    Revert "Sadhana - level design, adding materials"
    This reverts commit 6aa17d0ca10c95aac16af3ca3d9dc2737c6f7916.

--- Assets/Main/Scripts/PlayerController.cs
+++ Assets/Main/Scripts/PlayerController.cs
@@ -47,10 +47,6 @@ public class PlayerController : MonoBehaviour
         {
             speed += 2f; // increasing the player speed for sprinting 
         }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            speed -= 2f;
-        }
 
         characterController.Move(moveDirection * speed * Time.deltaTime);
 
@@ -130,18 +126,9 @@ public class PlayerController : MonoBehaviour
 
     public void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if(hit.gameObject.tag == "Platform" || hit.gameObject.tag == "MarianaTrench" || hit.gameObject.tag == "movePlatform")
+        if(hit.gameObject.tag == "Platform")
         {
             isJumping = false; // checking if player is on the platform and changing the bool value of isJumping
-            Debug.Log("Landed on Platform");
-        }
-        if(hit.gameObject.tag == "movePlatform")
-        {
-            this.transform.SetParent(hit.gameObject.transform);
-        }
-        else
-        {
-            this.transform.SetParent(null);
         }
     }
 
