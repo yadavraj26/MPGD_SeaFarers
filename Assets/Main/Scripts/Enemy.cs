52eff718d3acf08cfddbdb1fa8cf8773283b112d
ec22180 <agra.vasundhara@gmail.com>
2022-11-10 11:23:06 +0000

    enemy added

---
+++ Assets/Main/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent navAgent;
+    GameObject player;
+    private PlayerHealth playerHealth;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = GetComponent<PlayerHealth>();
+        navAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+        
+        if (distance <= navAgent.stoppingDistance) 
+        {
+            playerHealth.DecreaseHealth(20);
+            Debug.Log(playerHealth.maxHealth);
+        }
+
+        if ( distance > navAgent.stoppingDistance) 
+        {
+            navAgent.SetDestination(player.transform.position);
+        }
+    }
+
+    
+    }
