4ddd12b9c319d69b264f11801b376c0508e0848e
rahulyadav2000 <46553939+rahulyadav2000@users.noreply.github.com>
2023-01-03 11:00:52 +0000

    Loading screen and story added

---
+++ Assets/Main/Scripts/UI/LoadingScreen.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Slider slider;
+    public GameObject loadingScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(int sceneID)
+    {
+        StartCoroutine(LoadSceneAsync(sceneID));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneID)
+    {
+        AsyncOperation oper = SceneManager.LoadSceneAsync(sceneID);
+        loadingScreen.SetActive(true);
+
+        while (!oper.isDone)
+        {
+            float progress = Mathf.Clamp01(oper.progress/0.9f);
+
+            slider.value = progress;
+
+            yield return null;
+        }
+
+    }
+}
